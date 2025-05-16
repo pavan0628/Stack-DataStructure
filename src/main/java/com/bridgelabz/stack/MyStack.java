beca4780bc971ef615174729463b6f35535610d9
pavan0628 <pavan.cse.rymec@gmail.com>
2025-05-16 11:55:42 +0530

    [PAVAN]UPDATED:Implemented push method in my stack using linked list

--- src/main/java/com/bridgelabz/stack/MyStack.java
+++ src/main/java/com/bridgelabz/stack/MyStack.java
@@ -3,6 +3,7 @@ package com.bridgelabz.stack;
 public class MyStack {
     static SinglyLinkedList list=new SinglyLinkedList();
 
+    //push into the stack
     <E extends Comparable>void push(E data){
         list.addFirst(data);
     }
